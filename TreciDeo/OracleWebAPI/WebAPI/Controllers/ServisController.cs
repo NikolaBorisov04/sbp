0d21f69cac57e23c24ff0daa4c7de2324acefad0
Nikola Borisov <nikola.borisov2004@gmail.com>
2026-09-03 19:39:45 +0200

    Servis kontroler vise nema nepotrebne try catch blokove

--- TreciDeo/OracleWebAPI/WebAPI/Controllers/ServisController.cs
+++ TreciDeo/OracleWebAPI/WebAPI/Controllers/ServisController.cs
@@ -17,15 +17,8 @@ namespace WebAPI.Controllers
                 return BadRequest(new { greska = "Nevalidan ID vozila." });
             }
 
-            try
-            {
-                var rez = DTOManager.vratiSveServise(voziloId);
-                return Ok(rez);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { greska = ex.Message });
-            }
+            var rez = DTOManager.vratiSveServise(voziloId);
+            return Ok(rez);
         }
 
         [HttpGet]
@@ -37,20 +30,13 @@ namespace WebAPI.Controllers
                 return BadRequest(new { greska = "Nevalidan ID servisa." });
             }
 
-            try
+            var rez = DTOManager.vratiServis(id);
+            if (rez == null)
             {
-                var rez = DTOManager.vratiServis(id);
-                if (rez == null)
-                {
-                    return NotFound(new { greska = "Servis sa zadatim ID-jem nije pronađen." });
-                }
+                return NotFound(new { greska = "Servis sa zadatim ID-jem nije pronađen." });
+            }
 
-                return Ok(rez);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { greska = ex.Message });
-            }
+            return Ok(rez);
         }
 
         [HttpPost]
@@ -72,34 +58,27 @@ namespace WebAPI.Controllers
                 return BadRequest(new { greska = "Nevalidan ID tipa servisa." });
             }
 
-            try
+            ServisBasic sb_internal = new ServisBasic
             {
-                ServisBasic sb_internal = new ServisBasic
-                {
-                    Id = 0,
-                    VoziloId = sb.VoziloId,
-                    TipServisaId = sb.TipServisaId,
-                    ServisniCentar = sb.ServisniCentar,
-                    DatumPrijema = sb.DatumPrijema,
-                    DatumZavrsetka = sb.DatumZavrsetka,
-                    OpisRadova = sb.OpisRadova,
-                    ZamenjeniDelovi = sb.ZamenjeniDelovi,
-                    Troskovi = sb.Troskovi,
-                    Status = sb.Status
-                };
+                Id = 0,
+                VoziloId = sb.VoziloId,
+                TipServisaId = sb.TipServisaId,
+                ServisniCentar = sb.ServisniCentar,
+                DatumPrijema = sb.DatumPrijema,
+                DatumZavrsetka = sb.DatumZavrsetka,
+                OpisRadova = sb.OpisRadova,
+                ZamenjeniDelovi = sb.ZamenjeniDelovi,
+                Troskovi = sb.Troskovi,
+                Status = sb.Status
+            };
 
-                bool uspesno = DTOManager.dodajServis(sb_internal);
-                if (!uspesno)
-                {
-                    return BadRequest(new { greska = "Došlo je do greške prilikom evidentiranja servisa." });
-                }
-
-                return Ok(new { poruka = "Servis je uspešno evidentiran." });
-            }
-            catch (Exception ex)
+            bool uspesno = DTOManager.dodajServis(sb_internal);
+            if (!uspesno)
             {
-                return BadRequest(new { greska = ex.Message });
+                return BadRequest(new { greska = "Došlo je do greške prilikom evidentiranja servisa." });
             }
+
+            return Ok(new { poruka = "Servis je uspešno evidentiran." });
         }
 
         [HttpPut]
@@ -121,34 +100,27 @@ namespace WebAPI.Controllers
                 return BadRequest(new { greska = "ID u URL-u i u body-ju se ne poklapaju." });
             }
 
-            try
+            ServisBasic sb_internal = new ServisBasic
             {
-                ServisBasic sb_internal = new ServisBasic
-                {
-                    Id = sb.Id,
-                    VoziloId = sb.VoziloId,
-                    TipServisaId = sb.TipServisaId,
-                    ServisniCentar = sb.ServisniCentar,
-                    DatumPrijema = sb.DatumPrijema,
-                    DatumZavrsetka = sb.DatumZavrsetka,
-                    OpisRadova = sb.OpisRadova,
-                    ZamenjeniDelovi = sb.ZamenjeniDelovi,
-                    Troskovi = sb.Troskovi,
-                    Status = sb.Status
-                };
+                Id = sb.Id,
+                VoziloId = sb.VoziloId,
+                TipServisaId = sb.TipServisaId,
+                ServisniCentar = sb.ServisniCentar,
+                DatumPrijema = sb.DatumPrijema,
+                DatumZavrsetka = sb.DatumZavrsetka,
+                OpisRadova = sb.OpisRadova,
+                ZamenjeniDelovi = sb.ZamenjeniDelovi,
+                Troskovi = sb.Troskovi,
+                Status = sb.Status
+            };
 
-                bool uspesno = DTOManager.azurirajServis(sb_internal);
-                if (!uspesno)
-                {
-                    return BadRequest(new { greska = "Došlo je do greške prilikom ažuriranja servisa." });
-                }
-
-                return Ok(new { poruka = "Servis je uspešno ažuriran." });
-            }
-            catch (Exception ex)
+            bool uspesno = DTOManager.azurirajServis(sb_internal);
+            if (!uspesno)
             {
-                return BadRequest(new { greska = ex.Message });
+                return BadRequest(new { greska = "Došlo je do greške prilikom ažuriranja servisa." });
             }
+
+            return Ok(new { poruka = "Servis je uspešno ažuriran." });
         }
 
         [HttpDelete]
@@ -160,35 +132,21 @@ namespace WebAPI.Controllers
                 return BadRequest(new { greska = "Nevalidan ID servisa." });
             }
 
-            try
+            bool uspesno = DTOManager.obrisiServis(id);
+            if (!uspesno)
             {
-                bool uspesno = DTOManager.obrisiServis(id);
-                if (!uspesno)
-                {
-                    return NotFound(new { greska = "Servis sa zadatim ID-jem nije pronađen." });
-                }
+                return NotFound(new { greska = "Servis sa zadatim ID-jem nije pronađen." });
+            }
 
-                return Ok(new { poruka = "Servis je uspešno obrisan." });
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { greska = ex.Message });
-            }
+            return Ok(new { poruka = "Servis je uspešno obrisan." });
         }
 
         [HttpGet]
         [Route("katalog/tipovi-servisa")]
         public IActionResult vratiSveTipoveServisa()
         {
-            try
-            {
-                var rez = DTOManager.vratiSveTipoveServisa();
-                return Ok(rez);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { greska = ex.Message });
-            }
+            var rez = DTOManager.vratiSveTipoveServisa();
+            return Ok(rez);
         }
     }
 }
